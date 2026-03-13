9803284994fafce26eaedd289f2c64baaadb33c5
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2026-03-13 21:15:18 +0800

    build: enable R8 optimizations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,6 +62,8 @@ android {
                     value = "PiliPlus dev",
                 )
             }
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
